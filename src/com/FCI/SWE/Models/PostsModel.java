b5d155c02cf2905190f038911a236ed398d952eb
esraa <esraakhaled994@gmail.com>
2015-03-21 14:29:55 +0200

    updates in class structure

---
+++ src/com/FCI/SWE/Models/PostsModel.java
@@ -0,0 +1,30 @@
+package com.FCI.SWE.Models;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.osgi.framework.hooks.service.FindHook;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
+import com.google.appengine.api.datastore.Query.CompositeFilter;
+import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
+import com.googlecode.objectify.ObjectifyService;
+
+public class PostsModel{
+    
+}
---
+++ src/com/FCI/SWE/Services/PostsServices.java
@@ -0,0 +1,44 @@
+package com.FCI.SWE.Services;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.server.mvc.Viewable;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.FCI.SWE.Models.UserEntity;
+
+/**
+ * This class contains REST services for posts, also contains action function for web
+ * application
+ *
+ * @author Mohamed Samir
+ * @version 1.0
+ * @since 2014-02-12
+ * edited by esraa khaled
+ */
+@Path("/rest/posts/")
+@Produces(MediaType.TEXT_PLAIN)
+public class PostsServices{
+    
+}
