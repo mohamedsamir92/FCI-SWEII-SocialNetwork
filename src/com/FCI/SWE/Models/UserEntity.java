685aebce581809ff94acf35d875353539acd37dd
esraa <esraakhaled994@gmail.com>
2015-03-21 14:00:43 +0200

    search users by name

--- src/com/FCI/SWE/Models/UserEntity.java
+++ src/com/FCI/SWE/Models/UserEntity.java
@@ -36,168 +36,181 @@ import com.googlecode.objectify.ObjectifyService;
  * @since 2014-02-12
  */
 public class UserEntity {
-	private String name;
-	private String email;
-	private String password;
 
-	/**
-	 * Constructor accepts user data
-	 * 
-	 * @param name
-	 *            user name
-	 * @param email
-	 *            user email
-	 * @param password
-	 *            user provided password
-	 */
-	public UserEntity(String name, String email, String password) {
-		this.name = name;
-		this.email = email;
-		this.password = password;
+    private String name;
+    private String email;
+    private String password;
 
-	}
+    /**
+     * Constructor accepts user data
+     *
+     * @param name user name
+     * @param email user email
+     * @param password user provided password
+     */
+    public UserEntity(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
 
-	public String getName() {
-		return name;
-	}
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getPass() {
-		return password;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	/**
-	 * 
-	 * This static method will form UserEntity class using json format contains
-	 * user data
-	 * 
-	 * @param json
-	 *            String in json format contains user data
-	 * @return Constructed user entity
-	 */
-	public static UserEntity getUser(String json) {
+    public String getPass() {
+        return password;
+    }
 
-		JSONParser parser = new JSONParser();
-		try {
-			JSONObject object = (JSONObject) parser.parse(json);
-			return new UserEntity(object.get("name").toString(), object.get(
-					"email").toString(), object.get("password").toString());
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
+    /**
+     *
+     * This static method will form UserEntity class using json format contains
+     * user data
+     *
+     * @param json String in json format contains user data
+     * @return Constructed user entity
+     */
+    public static UserEntity getUser(String json) {
 
-	}
+        JSONParser parser = new JSONParser();
+        try {
+            JSONObject object = (JSONObject) parser.parse(json);
+            return new UserEntity(object.get("name").toString(), object.get(
+                    "email").toString(), object.get("password").toString());
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
 
-	/**
-	 * 
-	 * This static method will form UserEntity class using user name and
-	 * password This method will serach for user in datastore
-	 * 
-	 * @param name
-	 *            user name
-	 * @param pass
-	 *            user password
-	 * @return Constructed user entity
-	 */
+    }
 
-	public static UserEntity getUser(String name, String pass) {
-		DatastoreService datastore = DatastoreServiceFactory
-				.getDatastoreService();
+    /**
+     *
+     * This static method will form UserEntity class using user name and
+     * password This method will serach for user in datastore
+     *
+     * @param name user name
+     * @param pass user password
+     * @return Constructed user entity
+     */
+    public static UserEntity getUser(String name, String pass) {
+        DatastoreService datastore = DatastoreServiceFactory
+                .getDatastoreService();
 
-		Query gaeQuery = new Query("users");
-		PreparedQuery pq = datastore.prepare(gaeQuery);
-		for (Entity entity : pq.asIterable()) {
-			System.out.println(entity.getProperty("name").toString());
-			if (entity.getProperty("name").toString().equals(name)
-					&& entity.getProperty("password").toString().equals(pass)) {
-				UserEntity returnedUser = new UserEntity(entity.getProperty(
-						"name").toString(), entity.getProperty("email")
-						.toString(), entity.getProperty("password").toString());
-				return returnedUser;
-			}
-		}
-		return null;
-	}
-	
-	
-	public static UserEntity getUserByEMail(String email){
-		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-		Key key = KeyFactory.createKey("users", email);
-		Filter filter = 
-			    new FilterPredicate(Entity.KEY_RESERVED_PROPERTY,FilterOperator.EQUAL,key);
-		Query q = new Query("users").setFilter(filter);
-		
+        Query gaeQuery = new Query("users");
+        PreparedQuery pq = datastore.prepare(gaeQuery);
+        for (Entity entity : pq.asIterable()) {
+            System.out.println(entity.getProperty("name").toString());
+            if (entity.getProperty("name").toString().equals(name)
+                    && entity.getProperty("password").toString().equals(pass)) {
+                UserEntity returnedUser = new UserEntity(entity.getProperty(
+                        "name").toString(), entity.getProperty("email")
+                        .toString(), entity.getProperty("password").toString());
+                return returnedUser;
+            }
+        }
+        return null;
+    }
 
-		PreparedQuery res = datastore.prepare(q);		
-		Entity e = res.asSingleEntity();
-		if(e == null)return null;
-		UserEntity returnedUser = new UserEntity(e.getProperty(
-				"name").toString(), e.getProperty("email")
-				.toString(), e.getProperty("password").toString());
-		return returnedUser;
-	}
-	
-	
-	/**
-	 * This method will be used to save user object in datastore
-	 * 
-	 * @return boolean if user is saved correctly or not
-	 */
-	public Boolean saveUser() {
-		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-		Query gaeQuery = new Query("users");
-		PreparedQuery pq = datastore.prepare(gaeQuery);
-		List<Entity> list = pq.asList(FetchOptions.Builder.withDefaults());
+    public static UserEntity getUserByEMail(String email) {
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        Key key = KeyFactory.createKey("users", email);
+        Filter filter
+                = new FilterPredicate(Entity.KEY_RESERVED_PROPERTY, FilterOperator.EQUAL, key);
+        Query q = new Query("users").setFilter(filter);
 
-		Entity employee = new Entity("users", this.email);
+        PreparedQuery res = datastore.prepare(q);
+        Entity e = res.asSingleEntity();
+        if (e == null) {
+            return null;
+        }
+        UserEntity returnedUser = new UserEntity(e.getProperty(
+                "name").toString(), e.getProperty("email")
+                .toString(), e.getProperty("password").toString());
+        return returnedUser;
+    }
 
-		employee.setProperty("name", this.name);
-		employee.setProperty("email", this.email);
-		employee.setProperty("password", this.password);
-		datastore.put(employee);
+    public static UserEntity getUserByName(String name) {
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        Key key = KeyFactory.createKey("users", name);
+        Filter filter
+                = new FilterPredicate(Entity.KEY_RESERVED_PROPERTY, FilterOperator.EQUAL, key);
+        Query q = new Query("users").setFilter(filter);
 
-		return true;
+        PreparedQuery res = datastore.prepare(q);
+        Entity e = res.asSingleEntity();
+        if (e == null) {
+            return null;
+        }
+        UserEntity returnedUser = new UserEntity(e.getProperty(
+                "name").toString(), e.getProperty("email")
+                .toString(), e.getProperty("password").toString());
+        return returnedUser;
+    }
 
-	}
-	
-	public static boolean sendFriendRequest(String u1, String u2){
-		if(getUserByEMail(u1)==null || getUserByEMail(u2)== null){
-			return false;
-		}
-		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-		Entity request = new Entity("friendRequests",u1+"&"+u2);
-		request.setProperty("sender", u1);
-		request.setProperty("receiver", u2);
-		datastore.put(request);
-		return true;
-	}
-	
-	public static boolean acceptFriendRequest(String u1, String u2){
-		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-		
-		Key key = KeyFactory.createKey("friendRequests", u1+"&"+u2);
-		Filter filter = 
-			    new FilterPredicate(Entity.KEY_RESERVED_PROPERTY,FilterOperator.EQUAL,key);
-		Query q = new Query("friendRequests").setFilter(filter);
-		PreparedQuery res = datastore.prepare(q);
-		Entity e = res.asSingleEntity();
-		
-		if(e == null)return false;
-		datastore.delete(e.getKey());
-		
-		Entity a = new Entity("friends",u1+"&"+u2);
-		a.setProperty("u1", u1);
-		a.setProperty("u2", u2);
-		datastore.put(a);
+    /**
+     * This method will be used to save user object in datastore
+     *
+     * @return boolean if user is saved correctly or not
+     */
+    public Boolean saveUser() {
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        Query gaeQuery = new Query("users");
+        PreparedQuery pq = datastore.prepare(gaeQuery);
+        List<Entity> list = pq.asList(FetchOptions.Builder.withDefaults());
 
-		return true;
-	}
-	
+        Entity employee = new Entity("users", this.email);
+
+        employee.setProperty("name", this.name);
+        employee.setProperty("email", this.email);
+        employee.setProperty("password", this.password);
+        datastore.put(employee);
+
+        return true;
+
+    }
+
+    public static boolean sendFriendRequest(String u1, String u2) {
+        if (getUserByEMail(u1) == null || getUserByEMail(u2) == null) {
+            return false;
+        }
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        Entity request = new Entity("friendRequests", u1 + "&" + u2);
+        request.setProperty("sender", u1);
+        request.setProperty("receiver", u2);
+        datastore.put(request);
+        return true;
+    }
+
+    public static boolean acceptFriendRequest(String u1, String u2) {
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+        Key key = KeyFactory.createKey("friendRequests", u1 + "&" + u2);
+        Filter filter
+                = new FilterPredicate(Entity.KEY_RESERVED_PROPERTY, FilterOperator.EQUAL, key);
+        Query q = new Query("friendRequests").setFilter(filter);
+        PreparedQuery res = datastore.prepare(q);
+        Entity e = res.asSingleEntity();
+
+        if (e == null) {
+            return false;
+        }
+        datastore.delete(e.getKey());
+
+        Entity a = new Entity("friends", u1 + "&" + u2);
+        a.setProperty("u1", u1);
+        a.setProperty("u2", u2);
+        datastore.put(a);
+
+        return true;
+    }
+
 //	public static void search(String u1){
 //		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 //		Filter f = new FilterPredicate("u1",FilterOperator.EQUAL,u1);
--- src/com/FCI/SWE/Services/Service.java
+++ src/com/FCI/SWE/Services/Service.java
@@ -31,7 +31,7 @@ import com.FCI.SWE.Models.UserEntity;
 /**
  * This class contains REST services, also contains action function for web
  * application
- * 
+ *
  * @author Mohamed Samir
  * @version 1.0
  * @since 2014-02-12
@@ -40,105 +40,114 @@ import com.FCI.SWE.Models.UserEntity;
 @Path("/")
 @Produces(MediaType.TEXT_PLAIN)
 public class Service {
-	
-	
-	/*@GET
-	@Path("/index")
-	public Response index() {
-		return Response.ok(new Viewable("/jsp/entryPoint")).build();
-	}*/
 
+    /*@GET
+     @Path("/index")
+     public Response index() {
+     return Response.ok(new Viewable("/jsp/entryPoint")).build();
+     }*/
+    /**
+     * Registration Rest service, this service will be called to make
+     * registration. This function will store user data in data store
+     *
+     * @param uname provided user name
+     * @param email provided user email
+     * @param pass provided password
+     * @return Status json
+     */
+    @POST
+    @Path("/register")
+    public String registrationService(@FormParam("uname") String uname,
+            @FormParam("email") String email, @FormParam("password") String pass) {
+        UserEntity user = new UserEntity(uname, email, pass);
+        user.saveUser();
+        JSONObject object = new JSONObject();
+        object.put("Status", "OK");
+        return object.toString();
+    }
 
-		/**
-	 * Registration Rest service, this service will be called to make
-	 * registration. This function will store user data in data store
-	 * 
-	 * @param uname
-	 *            provided user name
-	 * @param email
-	 *            provided user email
-	 * @param pass
-	 *            provided password
-	 * @return Status json
-	 */
-	@POST
-	@Path("/register")
-	public String registrationService(@FormParam("uname") String uname,
-			@FormParam("email") String email, @FormParam("password") String pass) {
-		UserEntity user = new UserEntity(uname, email, pass);
-		user.saveUser();
-		JSONObject object = new JSONObject();
-		object.put("Status", "OK");
-		return object.toString();
-	}
+    /**
+     * Login Rest Service, this service will be called to make login process
+     * also will check user data and returns new user from datastore
+     *
+     * @param uname provided user name
+     * @param pass provided user password
+     * @return user in json format
+     */
+    @POST
+    @Path("/login")
+    public String loginService(@FormParam("email") String email,
+            @FormParam("password") String pass) {
+        JSONObject object = new JSONObject();
+        UserEntity user = UserEntity.getUserByEMail(email);
+        if (user == null || !user.getPass().equals(pass)) {
+            object.put("Status", "Failed");
 
-	/**
-	 * Login Rest Service, this service will be called to make login process
-	 * also will check user data and returns new user from datastore
-	 * @param uname provided user name
-	 * @param pass provided user password
-	 * @return user in json format
-	 */
-	@POST
-	@Path("/login")
-	public String loginService(@FormParam("email") String email,
-			@FormParam("password") String pass) {
-		JSONObject object = new JSONObject();
-		UserEntity user = UserEntity.getUserByEMail(email);
-		if (user == null || !user.getPass().equals(pass)) {
-			object.put("Status", "Failed");
+        } else {
+            object.put("Status", "OK");
+            object.put("name", user.getName());
+            object.put("email", user.getEmail());
+            object.put("password", user.getPass());
+        }
 
-		} else {
-			object.put("Status", "OK");
-			object.put("name", user.getName());
-			object.put("email", user.getEmail());
-			object.put("password", user.getPass());
-		}
+        return object.toString();
 
-		return object.toString();
+    }
 
-	}
-	
-	@POST
-	@Path("/sendFriendRequest")
-	public String sendFriendRequest(@FormParam("user_one") String user_one,
-									@FormParam("user_two") String user_two){
-		JSONObject obj = new JSONObject();
-		if(UserEntity.sendFriendRequest(user_one,user_two)){
-			obj.put("Status", "OK");
-		}else{
-			obj.put("Status", "Failed");
-		}
-		return obj.toString();
-	}
-	
-	@POST
-	@Path("/acceptFriendRequest")
-	public String acceptFriendRequest(@FormParam("user_one")String user_one,
-									@FormParam("user_two")String user_two){
-		JSONObject obj = new JSONObject();
-		if(UserEntity.acceptFriendRequest(user_one,user_two)){
-			obj.put("Status" , "OK");
-		}else{
-			obj.put("Status", "Failed");
-		}
-		return obj.toString();
-	}
-	
-	@GET
-	@Path("/getUserByEMail/{email}")
-	public String getUserByEMail(@PathParam("email") String email){
-		JSONObject obj = new JSONObject();
-		UserEntity u = UserEntity.getUserByEMail(email);		
-		if(u==null){
-			obj.put("Status" , "Failed");
-		}else{
-			obj.put("Status", "OK");
-			obj.put("name", u.getName());
-			obj.put("email", u.getEmail());
-		}
-		return obj.toString();
-	}
-	
-	
+    @POST
+    @Path("/sendFriendRequest")
+    public String sendFriendRequest(@FormParam("user_one") String user_one,
+            @FormParam("user_two") String user_two) {
+        JSONObject obj = new JSONObject();
+        if (UserEntity.sendFriendRequest(user_one, user_two)) {
+            obj.put("Status", "OK");
+        } else {
+            obj.put("Status", "Failed");
+        }
+        return obj.toString();
+    }
+
+    @POST
+    @Path("/acceptFriendRequest")
+    public String acceptFriendRequest(@FormParam("user_one") String user_one,
+            @FormParam("user_two") String user_two) {
+        JSONObject obj = new JSONObject();
+        if (UserEntity.acceptFriendRequest(user_one, user_two)) {
+            obj.put("Status", "OK");
+        } else {
+            obj.put("Status", "Failed");
+        }
+        return obj.toString();
+    }
+
+    @GET
+    @Path("/getUserByEMail/{email}")
+    public String getUserByEMail(@PathParam("email") String email) {
+        JSONObject obj = new JSONObject();
+        UserEntity u = UserEntity.getUserByEMail(email);
+        if (u == null) {
+            obj.put("Status", "Failed");
+        } else {
+            obj.put("Status", "OK");
+            obj.put("name", u.getName());
+            obj.put("email", u.getEmail());
+        }
+        return obj.toString();
+    }
+
+    @GET
+    @Path("/searchUsersByName/{name}")
+    public String getUserByName(@PathParam("name") String name) {
+        JSONObject obj = new JSONObject();
+        UserEntity u = UserEntity.getUserByEMail(name);
+        if (u == null) {
+            obj.put("Status", "Failed");
+        } else {
+            obj.put("Status", "OK");
+            obj.put("name", u.getName());
+            obj.put("email", u.getEmail());
+        }
+        return obj.toString();
+    }
+
 }
