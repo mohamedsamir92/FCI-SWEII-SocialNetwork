584bacae6d9c98d00e495313ee3012caeff2d25d
halamohamed199 <hala.mohamed199@gmail.com>
2015-04-24 01:28:46 +0300

    modified userPost/freindPost

--- src/com/FCI/SWE/Services/UserPost.java
+++ src/com/FCI/SWE/Services/UserPost.java
@@ -4,7 +4,9 @@ import static com.FCI.SWE.Models.OfyService.ofy;
 
 import java.util.ArrayList;
 
+import javax.validation.constraints.Past;
 import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
 import com.FCI.SWE.Models.Friends;
@@ -12,7 +14,7 @@ import com.FCI.SWE.Models.PostsModel;
 import com.FCI.SWE.Models.UserEntity;
 import com.google.appengine.labs.repackaged.org.json.JSONException;
 import com.google.appengine.labs.repackaged.org.json.JSONObject;
-
+@Path("/post")
 public class UserPost extends PostsModel {
 	static private String status = "Status";
 	static private String ok = "OK";
@@ -26,7 +28,8 @@ public class UserPost extends PostsModel {
 	 
 	public void save(){
 			ofy().save().entity(this);
-		}
+	}
+	
 	public UserPost(String sender,String text,String feeling,String privacy) {
 		super(text);
 		this.sender=sender;
@@ -37,6 +40,7 @@ public class UserPost extends PostsModel {
 		super(text);
 		this.receiver=receiver;
 	}
+	@POST
 	@Path("/userPost/")
 	public String userPost(@FormParam("email") String email,
 			@FormParam("text") String text,@FormParam("feeling") String feeling,
@@ -60,6 +64,8 @@ public class UserPost extends PostsModel {
 		return  obj.put(status,ok).toString();
 		
 	}
+	@POST
+	@Path("/freindPost/")
 	public String freindPost(@FormParam("sender") String sender,
 			@FormParam("receiver") String receiver,
 			@FormParam("text") String text) throws JSONException{
